2a1af733c26d4880f9327b360faf9d4667a1da27
Emerson Junior <emerson.c.junior.115@gmail.com>
2025-05-26 15:34:15 -0300

    função de teste serverless function

--- api/test.js
+++ api/test.js
@@ -1,5 +1,4 @@
-
-export default function handler(res) {
+export default function handler(req, res) {
   console.log("Função rodando normalmente!");
   res.status(200).json({ message: "Função rodando normalmente!" });
 }
